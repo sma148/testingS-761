7614db72b87f8e5e4586638781d7911039747b48
sma148 <sma148@aucklanduni.ac.nz>
2023-09-02 09:13:45 +1200

    added models

---
+++ testingS761/Model/ColumnData.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace testingS761.Model
+{
+    public class ColumnData
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Value { get; set; }
+    }
+}
---
+++ testingS761/Model/Row.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace testingS761.Model
+{
+    public class Row
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public List<ColumnData> Columns { get; set; }
+    }
+}
